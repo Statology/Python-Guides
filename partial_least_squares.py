f2455f73a33f490d0d3d10ed1f8a9cb2b41136c9
Statology <72817609+Statology@users.noreply.github.com>
2020-11-17 12:36:55 -0500

    Update partial_least_squares.py

--- partial_least_squares.py
+++ partial_least_squares.py
@@ -24,30 +24,30 @@ data[0:6]
 X = data[["mpg", "disp", "drat", "wt", "qsec"]]
 y = data[["hp"]]
 
-#split the dataset into training (70%) and testing (30%) sets
-X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.3,random_state=0) 
-
-n = len(X_train)
-
 #define cross-validation method
 cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=1)
 
 mse = []
+n = len(X)
 
 # Calculate MSE with only the intercept
-score = -1*model_selection.cross_val_score(PLSRegression(n_components=1), np.ones((n,1)), y_train, cv=cv, scoring='neg_mean_squared_error').mean()    
+score = -1*model_selection.cross_val_score(PLSRegression(n_components=1), np.ones((n,1)), y, cv=cv, scoring='neg_mean_squared_error').mean()    
 mse.append(score)
 
 # Calculate MSE using cross-validation, adding one component at a time
 for i in np.arange(1, 6):
     pls = PLSRegression(n_components=i)
-    score = -1*model_selection.cross_val_score(pls, scale(X_train), y_train, cv=cv, scoring='neg_mean_squared_error').mean()
+    score = -1*model_selection.cross_val_score(pls, scale(X), y, cv=cv, scoring='neg_mean_squared_error').mean()
     mse.append(score)
 
+#plot test MSE vs. number of components
 plt.plot(mse)
-plt.xlabel('Number of Principal Components')
+plt.xlabel('Number of PLS Components')
 plt.ylabel('MSE')
 plt.title('hp')
+
+#split the dataset into training (70%) and testing (30%) sets
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.3,random_state=0) 
 
 #calculate RMSE
 pls = PLSRegression(n_components=2)
