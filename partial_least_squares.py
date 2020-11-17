7a82826fb548662d0236e449b3cf837bbe87fd23
Statology <72817609+Statology@users.noreply.github.com>
2020-11-17 12:22:17 -0500

    Add files via upload

---
+++ partial_least_squares.py
@@ -0,0 +1,55 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import scale 
+from sklearn import model_selection
+from sklearn.model_selection import RepeatedKFold
+from sklearn.model_selection import train_test_split
+from sklearn.cross_decomposition import PLSRegression
+from sklearn.metrics import mean_squared_error
+
+#define URL where data is located
+url = "https://raw.githubusercontent.com/Statology/Python-Guides/main/mtcars.csv"
+
+#read in data
+data_full = pd.read_csv(url)
+
+#select subset of data
+data = data_full[["mpg", "disp", "drat", "wt", "qsec", "hp"]]
+
+#view first six rows of data
+data[0:6]
+#define predictor and response variables
+X = data[["mpg", "disp", "drat", "wt", "qsec"]]
+y = data[["hp"]]
+
+#split the dataset into training (70%) and testing (30%) sets
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.3,random_state=0) 
+
+n = len(X_train)
+
+#define cross-validation method
+cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=1)
+
+mse = []
+
+# Calculate MSE with only the intercept
+score = -1*model_selection.cross_val_score(PLSRegression(n_components=1), np.ones((n,1)), y_train, cv=cv, scoring='neg_mean_squared_error').mean()    
+mse.append(score)
+
+# Calculate MSE using cross-validation, adding one component at a time
+for i in np.arange(1, 6):
+    pls = PLSRegression(n_components=i)
+    score = -1*model_selection.cross_val_score(pls, scale(X_train), y_train, cv=cv, scoring='neg_mean_squared_error').mean()
+    mse.append(score)
+
+plt.plot(mse)
+plt.xlabel('Number of Principal Components')
+plt.ylabel('MSE')
+plt.title('hp')
+
+#calculate RMSE
+pls = PLSRegression(n_components=2)
+pls.fit(scale(X_train), y_train)
+
+np.sqrt(mean_squared_error(y_test, pls.predict(scale(X_test))))
